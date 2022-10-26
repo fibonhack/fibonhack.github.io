d87733d68b1375948f9391b7f396aac27a83adc7
just-Hms <alessandroversari1@gmail.com>
2022-10-26 17:59:28 +0200

    no-js class added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,14 @@
-window.onload = () =>{
+const copy_svg = `
+	<svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 16H6a2 2 0 01-2-2V6a2 2 0 012-2h8a2 2 0 012 2v2m-6 12h8a2 2 0 002-2v-8a2 2 0 00-2-2h-8a2 2 0 00-2 2v8a2 2 0 002 2z"></path></svg>
+`
+
+const copied_svg = `
+	<svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="w-6 h-6 text-green-500" viewBox="0 0 16 16">
+		<path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425a.247.247 0 0 1 .02-.022Z"/>
+	</svg>
+`
+
+const code_snippets = () => {
 
 	const snippets = document.querySelectorAll(".highlighter-rouge > .highlight");
 
@@ -7,6 +17,7 @@ window.onload = () =>{
 		s.classList.add("relative", "group");
 		
 		const copy_btn = document.createElement("div");
+
 		copy_btn.classList.add(
 			"absolute",
 			"hidden",
@@ -21,12 +32,7 @@ window.onload = () =>{
 			"hover:brightness-150"
 		);
 
-		copy_btn.innerHTML = `
-			<svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="currentColor" viewBox="0 0 16 16">
-				<path d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1v-1z"/>
-				<path d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5h3zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3z"/>
-			</svg>
-		`;
+		copy_btn.innerHTML = copy_svg;
 
 		s.appendChild(copy_btn);
 
@@ -38,24 +44,95 @@ window.onload = () =>{
 
 			navigator.clipboard.writeText(s.innerText);
 			
-			copy_btn.innerHTML = `
-				<svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="w-6 h-6 text-green-500" viewBox="0 0 16 16">
-					<path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425a.247.247 0 0 1 .02-.022Z"/>
-				</svg>
-			`;
+			copy_btn.innerHTML = copied_svg;
 
 			disabled = true;
 
 			setTimeout(() => {
-				copy_btn.innerHTML = `
-					<svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="currentColor" viewBox="0 0 16 16">
-						<path d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1v-1z"/>
-						<path d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5h3zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3z"/>
-					</svg>
-				`;
+				copy_btn.innerHTML = copy_svg;
 				disabled = false;
 			}, 1200);
 		}
 		
 	});
+}
+
+const terminal = () => {
+
+	let terminal = document.getElementById("command_form");
+
+	if(!terminal) return;
+
+	terminal.onsubmit = e => {
+		
+		e.preventDefault();
+		
+		const command = document.getElementById("command");
+
+		if (!command) return;
+
+		if (command.value === "cat flag.txt") {
+			alert("{{kek}}"); 
+			return;
+		} 
+
+		if (command.value === "cd /") {
+			location.href ="/";
+			return; 
+		}
+		if (command.value === "cd .."){
+			const location_divided = location.href.split("/"); 
+			
+			if (location_divided.length < 2) return; 
+			
+			location.href = location_divided.slice(0, location_divided.length - 2).join('/'); 
+			return; 
+		}
+
+		if (command.value.startsWith("cd /")){
+			location.href = "/" + command.value.split(" /")[1]; 
+			return; 
+		}
+
+		if (command.value.startsWith("cd ")){
+			location.href += command.value.split(" ")[1]; 
+			return; 
+		}
+	}
+
+}
+
+// remove animation-retard elemet view from the dom, 
+// in the css only version the retard is provided by changing the visibility property
+
+const hide_retards = () => {
+	const retards = document.getElementsByClassName("animation-retard");
+	
+	const retard = getComputedStyle(document.documentElement)?.getPropertyValue('--retard')?.replace("ms", "") || 900; 
+
+	[].forEach.call(retards, r => {
+		r.classList.add("hidden");
+	});
+
+	setTimeout(() => {
+		[].forEach.call(retards, r => {
+			r.classList.remove("hidden");
+		});
+	}, retard);
+	
+}
+
+window.onload = () => {
+
+	// remove no-js class, thus css will know that css is active
+	[].forEach.call(
+		document.getElementsByClassName("no-js"), 
+		no_js => {
+			no_js.classList.remove("no-js");
+		}
+	);
+
+	terminal();
+	code_snippets();
+	hide_retards();
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,22 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    container: {
+      // you can configure the container to be centered
+      center: true,
+
+      // or have default horizontal padding
+      padding: '1rem',
+
+      // default breakpoints but with 40px removed
+      screens: {
+        sm: '600px',
+        md: '728px',
+        lg: '984px',
+        // xl: '1240px',
+        // '2xl': '1496px',
+      },
+    },
   },
   variants: {},
   plugins: [
